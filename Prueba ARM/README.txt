384399a1628309e351fa0027d3c0af4429a5fa49
Diego Cali <diegocali123@gmail.com>
2024-06-19 17:27:38 -0600

    Testing with python

---
+++ Prueba ARM/README.txt
@@ -0,0 +1,4 @@
+Para generar el ejecutable con la automatizacion en bash usar el
+siguiente comando en consola:
+
+bash asm.sh [nombre archivo sin extension]
---
+++ Prueba ARM/UltraS.py
@@ -0,0 +1,12 @@
+import subprocess
+
+def sum_asm(num1, num2):
+    result = subprocess.run(['./assembler'], input=f'{num1}{num2}\n', text=True, capture_output=True)
+    return result.returncode
+
+array = [1, 2, 3, 4]
+print('Suma de numeros en asembler de:', array)
+total = 0
+for num in array:
+    total = sum_asm(total, num)
+print(f'La suma es: {total}')
