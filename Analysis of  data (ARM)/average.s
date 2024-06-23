8e3ff8c81a1ef4bc6388a38ceb6fa12fafb0fd25
Josue Guevara <3663830780101@ingenieria.usac.edu.gt>
2024-06-23 13:56:39 -0600

    promedio por lectura de archivo por medio de parametro
    se corrigio errores basicos en el calculo del promedio.
    Se puede pasar el nombre del archivo a analizar por medio de parametro para su calculo en arm

--- Analysis of  data (ARM)/average.s
+++ Analysis of  data (ARM)/average.s
@@ -1,21 +1,38 @@
 .global _start
 
+.bss 
+arg1: .space 32         // space for 32 caracters
+output: .skip 12        // space for 12 bytes
+buffer: .skip 1024      // space for 1024 bytes
+
 .data
-filename:
-    .asciz "nums.txt"
-
-.bss
-buffer:
-    .skip 1024
-
-output:
-    .skip 12
+newline: .asciz "\n"    // new line
 
 .text
 _start:
+    //argv[1] address
+    ldr x0, [sp, 16]    // load address of argv[1]
+    ldr x1, =arg1       // load address of arg1
+    mov x2, 0           //
+
+    // copy argv[1] to arg1
+loop_argv:
+    ldrb w3, [x0, x2]   // load byte
+    cmp w3, 0           // if null
+    beq end_loop_argv   // goto end_loop_argv
+    strb w3, [x1, x2]   // store byte
+    add x2, x2, 1       // increment counter
+    b loop_argv         // goto loop_argv
+
+    // add eof
+end_loop_argv:
+    mov w0, 0           // add eof
+    strb w0, [x1, x2]   // store byte
+
+open_file:
     // open file
     mov x0, -100        // open
-    ldr x1, =filename   // filename address
+    ldr x1, =arg1          // filename address
     mov x2, 0           // O_RDONLY 
     mov x8, 56          // openat
     svc #0              // syscall
@@ -33,13 +50,14 @@ _start:
     mov x8, 57          // close
     svc 0               // syscall
 
+set_variables:
     ldr x1, =buffer     // buffer address
     mov x3, 0           // number size = 0
     mov x4, 10          // base number 
     mov x5, 0           // sum
     mov x6, 0           // counter               
 
-    // loop
+    //casting number
 loop:
     ldrb w2, [x1]       // load byte
     cmp w2, 36          // if $
