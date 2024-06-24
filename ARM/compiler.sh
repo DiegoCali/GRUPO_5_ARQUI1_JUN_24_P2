f2a4377ab68edfeacc3f7f04d4a73c5ab2a628d5
Diego Cali <diegocali123@gmail.com>
2024-06-23 19:00:10 -0600

    Merge all features and ARM testing

--- ARM/compiler.sh
+++ ARM/compiler.sh
@@ -2,4 +2,5 @@
 
 as -o $1.o $1.s
 ld -o $1 $1.o
+rm $1.o
 ./$1 ; echo $?
--- Testing.py
+++ Testing.py
@@ -1,3 +1,4 @@
+import subprocess
 from Climate import Climate
 from time import sleep
 
@@ -5,6 +6,11 @@ new_climate = Climate()
 
 new_climate.read_climate()
 
+num = 2
+
 while True:
+    result = subprocess.run('./ARM/SquareRoot', input=f'{num}\n', text=True, capture_output=True)
+    print(result.stdout)
     print(new_climate.get_climate_data())
+    num = num * 2
     sleep(2)
