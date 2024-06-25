d7d9b8182398fd1898697d258517a4e8f49474d0
Diego Cali <diegocali123@gmail.com>
2024-06-24 20:42:24 -0600

    Calculator class

--- ARM/Calculator.py
+++ ARM/Calculator.py
@@ -1,3 +1,5 @@
+import subprocess as sp
+
 class Calculator:
 
     def __init__(self):
@@ -11,4 +13,36 @@ class Calculator:
             'contador': -1
         }
 
+    def get_data(self, variable):
+        pass
+
+    def get_average(self, variable):
+        result = sp.run([f'./average DB/{variable}.txt'], text=True, capture_output=True)
+        return result.stdout
     
+    def get_median(self, variable):
+        result = sp.run([f'./median DB/{variable}.txt'], text=True, capture_output=True)
+        return result.stdout
+    
+    def get_stnd_dev(self, variable):
+        sqaverage = sp.run([f'./sqaverage DB/{variable}.txt'], text=True, capture_output=True).stdout
+        average = sp.run([f'./sqaverage DB/{variable}.txt'], text=True, capture_output=True).stdout
+        variance = int(sqaverage) - int(average*average)
+        stnd_dev = sp.run([f'./SquareRoot DB/{str(variance)}.txt'], text=True, capture_output=True).stdout
+        return stnd_dev
+
+    def get_max(self, variable):
+        result = sp.run([f'./max DB/{variable}.txt'], text=True, capture_output=True)
+        return result.stdout
+
+    def get_min(self, variable):
+        result = sp.run([f'./mini DB/{variable}.txt'], text=True, capture_output=True)
+        return result.stdout
+
+    def get_mode(self, variable):
+        result = sp.run([f'./mode DB/{variable}.txt'], text=True, capture_output=True)
+        return result.stdout
+
+    def get_counter(self, variable):
+        result = sp.run([f'./counter DB/{variable}.txt'], text=True, capture_output=True)
+        return result.stdout
