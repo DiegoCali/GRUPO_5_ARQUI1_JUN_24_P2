aca9015ed4a8b592044da7181887bb3d64d29fb1
Diego Cali <diegocali123@gmail.com>
2024-06-24 19:07:54 -0600

    file writing

--- Tests/Testing.py
+++ Tests/Testing.py
@@ -5,12 +5,3 @@ from time import sleep
 new_climate = Climate()
 
 new_climate.read_climate()
-
-num = 2
-
-while True:
-    result = subprocess.run('./ARM/SquareRoot', input=f'{num}\n', text=True, capture_output=True)
-    print(result.stdout)
-    print(new_climate.get_climate_data())
-    num = num * 2
-    sleep(2)
